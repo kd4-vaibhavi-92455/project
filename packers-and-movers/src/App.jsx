12f1c802340b069fe11fd9792c27dc53e8612b6f
Vaibhavi Upreti <vaibhaviu.ind@gmail.com>
2026-01-21 16:36:33 +0530

    added folder structure

--- packers-and-movers/src/App.jsx
+++ packers-and-movers/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="bg-amber-400">
-      <p className="read-the-docs">Project basic structure</p>
+      <p className="read-the-docs dark text-primary">
+        Project basic structure..
+      </p>
     </div>
   );
 }
