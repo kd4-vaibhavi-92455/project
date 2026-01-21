e9c69d265dd522b6a26abc4523be4b6a5d032023
Vaibhavi Upreti <vaibhaviu.ind@gmail.com>
2026-01-21 17:09:08 +0530

    added layout and providers

--- packers-and-movers/src/App.jsx
+++ packers-and-movers/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import "./index.css";
+import ThemeProvider from "../providers/ThemeProvider";
 
 function App() {
   return (
-    <div className="bg-amber-400">
-      <p className="read-the-docs dark text-primary">
-        Project basic structure..
-      </p>
-    </div>
+    <ThemeProvider>
+      <div className="bg-amber-400">
+        <p className="read-the-docs dark text-primary">
+          Project basic structure..
+        </p>
+      </div>
+    </ThemeProvider>
   );
 }
 
