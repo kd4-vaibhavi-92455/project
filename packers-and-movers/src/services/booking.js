def9a96a976dbb46c3fcdbe3981c91cc3a0a9a28
Vaibhavi Upreti <vaibhaviu.ind@gmail.com>
2026-01-28 00:56:05 +0530

    added frontend-booking apis: create booking, request cancellation, booking cancellation,get current and cancelled bookings/past bookings, modify bookings for users

---
+++ packers-and-movers/src/services/booking.js
@@ -0,0 +1,65 @@
+// src/services/booking.service.js
+import axios from "axios";
+import { BASE_URL, getAuthHeader } from "./config";
+
+// CREATE BOOKING
+export async function createBooking(data) {
+  const response = await axios.post(`${BASE_URL}/users/bookings`, data, {
+    headers: getAuthHeader(),
+  });
+  return response.data;
+}
+
+// REQUEST CANCELLATION (request only)
+export async function requestBookingCancellation(bookingId, reason) {
+  const response = await axios.post(
+    `${BASE_URL}/users/bookings/${bookingId}/request-cancellation`,
+    reason,
+    { headers: getAuthHeader() },
+  );
+  return response.data;
+}
+
+// CANCEL BOOKING (final cancel)
+export async function cancelBooking(bookingId, reason) {
+  const response = await axios.put(
+    `${BASE_URL}/users/bookings/${bookingId}/cancel`,
+    reason,
+    { headers: getAuthHeader() },
+  );
+  return response.data;
+}
+
+// GET CURRENT BOOKINGS
+export async function getCurrentBookings() {
+  const response = await axios.get(`${BASE_URL}/users/bookings/current`, {
+    headers: getAuthHeader(),
+  });
+  return response.data;
+}
+
+// GET BOOKING HISTORY
+export async function getBookingHistory() {
+  const response = await axios.get(`${BASE_URL}/users/bookings/history`, {
+    headers: getAuthHeader(),
+  });
+  return response.data;
+}
+
+// UPDATE BOOKING BY ID
+export async function updateBooking(bookingId, data) {
+  const response = await axios.put(
+    `${BASE_URL}/users/bookings/${bookingId}`,
+    data,
+    { headers: getAuthHeader() },
+  );
+  return response.data;
+}
+
+// UPDATE USER PROFILE
+export async function updateUserProfile(profile) {
+  const response = await axios.put(`${BASE_URL}/users/profile`, profile, {
+    headers: getAuthHeader(),
+  });
+  return response.data;
+}
